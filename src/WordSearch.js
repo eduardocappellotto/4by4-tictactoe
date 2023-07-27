eb9508edee6b8449b52764554972db7070560d99
Devskiller <owl@devskiller.com>
2023-07-27 21:59:11 +0000

    Update src/WordSearch.js

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -4,89 +4,88 @@ export default class WordSearch {
   }
 
   find(words) {
- 
-  const rowsCount = this.letterGrid.length;
-  const colsCount = this.letterGrid[0].length;
+    const numRows = this.grid.length;
+    const numCols = this.grid[0].length;
+
+    // Helper function to check if a word can be found starting from a given position in a given direction
+    const checkWordInDirection = (row, col, word, direction) => {
+      let dr = direction[0];
+      let dc = direction[1];
+
+      // Check if the word can fit starting from the current position in the specified direction
+      if (
+        row + (word.length - 1) * dr < 0 ||
+        row + (word.length - 1) * dr >= numRows ||
+        col + (word.length - 1) * dc < 0 ||
+        col + (word.length - 1) * dc >= numCols
+      ) {
+        return false;
+      }
+
+      // Check if the word matches in the specified direction
+      for (let i = 0; i < word.length; i++) {
+        if (this.grid[row + i * dr][col + i * dc] !== word[i]) {
+          return false;
+        }
+      }
+
+      return true;
+    };
 
     const results = {};
 
-    for(const word of words){
-
+    // Iterate through each word in the array and search for it in the grid
+    for (const word of words) {
       let found = false;
       let start = null;
-      let end = null; 
+      let end = null;
 
-      
-      for(let r= 0; r < rowsCount; r++){
-        for(let c= 0; c < colsCount; c++){
-        // Will check on every direction
+      for (let r = 0; r < numRows; r++) {
+        for (let c = 0; c < numCols; c++) {
+          if (checkWordInDirection(r, c, word, [0, 1])) {
+            found = true;
+            start = [r, c];
+            end = [r, c + word.length - 1];
+            break;
+          }
 
-        if(this.checkWordInDirection(r, c, word, [0,1])){
-          found = true;
-          start = [r, c];
-          end = [r, c + word.length -1]
-          break;
+          if (checkWordInDirection(r, c, word, [0, -1])) {
+            found = true;
+            start = [r, c - word.length + 1];
+            end = [r, c];
+            break;
+          }
+
+          if (checkWordInDirection(r, c, word, [1, 0])) {
+            found = true;
+            start = [r, c];
+            end = [r + word.length - 1, c];
+            break;
+          }
+
+          if (checkWordInDirection(r, c, word, [-1, 0])) {
+            found = true;
+            start = [r - word.length + 1, c];
+            end = [r, c];
+            break;
+          }
         }
-        if(this.checkWordInDirection(r, c, word, [0,-1])){
-          found = true;
-          start = [r, c - word.length -1]
-          end =  [r, c];
-          break;
-        }
-        if(this.checkWordInDirection(r, c, word, [1,0])){
-          found = true;
-          start =[r, c]; 
-          end = [r + word.length - 1, c];
-          break;
-        }
-        if(this.checkWordInDirection(r, c, word, [-1,0])){
-          found = true;
-         start = [r - word.length +1, c];
-          end = [r, c];
+
+        if (found) {
           break;
         }
       }
 
-      if(found){
-        break;
-      }
-      }
-
-      if(found){
-        results[word] = { start, end}
+      if (found) {
+        results[word] = { start, end };
       } else {
-        results[word] = undefined
+        results[word] = undefined;
       }
     }
 
-    return results
-
-  };
-
-  checkWordInDirection(row, col, word, direction){
-    let dr = direction[0]
-    let dc = direction[1]
-
-    // Checks if the word can fit
-    if(
-      row + (word.length - 1) * dr < 0 ||
-      row + (word.length - 1) * dr >= rowsCount ||
-      col + (word.length - 1) * dc < 0 ||
-      col + (word.length - 1) * dc >= colsCount 
-    ){
-      return false
-    }
-
-  // Checks if the word matches in direction
-    for( let i=0; i< word.length; i++) {
-      if(this.letterGrid[row + i * dr][col + 1 * dc] !== word[i]){
-        return false
-      }
-    }
-
-    return true
-
-  };
+    return results;
+  }
+}
 
 
 
