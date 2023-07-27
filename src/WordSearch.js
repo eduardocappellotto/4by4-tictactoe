e4391d3f79dd2412e21bd7c2e7a27cb65c090711
Devskiller <owl@devskiller.com>
2023-07-27 21:56:41 +0000

    Update src/WordSearch.js

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -4,6 +4,101 @@ export default class WordSearch {
   }
 
   find(words) {
-    // TO-DO: Implement searching for specific words in the letterGrid
-  }
+ 
+  const rowsCount = this.letterGrid.length;
+  const colsCount = this.letterGrid[0].length;
+
+    const results = {};
+
+    for(const word of words){
+
+      let found = false;
+      let start = null;
+      let end = null; 
+
+      
+      for(let r= 0; r < rowsCount; r++){
+        for(let c= 0; c < colsCount; c++){
+        // Will check on every direction
+
+        if(checkWordInDirection(r, c, word, [0,1])){
+          found = true;
+          start = [r, c];
+          end = [r, c + word.length -1]
+          break;
+        }
+        if(checkWordInDirection(r, c, word, [0,-1])){
+          found = true;
+          start = [r, c - word.length -1]
+          end =  [r, c];
+          break;
+        }
+        if(checkWordInDirection(r, c, word, [1,0])){
+          found = true;
+          start =[r, c]; 
+          end = [r + word.length - 1, c];
+          break;
+        }
+        if(checkWordInDirection(r, c, word, [-1,0])){
+          found = true;
+         start = [r - word.length +1, c];
+          end = [r, c];
+          break;
+        }
+      }
+
+      if(found){
+        break;
+      }
+      }
+
+      if(found){
+        results[word] = { start, end}
+      } else {
+        results[word] = undefined
+      }
+    }
+
+    return results
+
+  };
+
+  checkWordInDirection(row, col, word, direction){
+    let dR = direction[0]
+    let dC = direction[1]
+
+    // Checks if the word can fit
+    if(
+      row + (word.length - 1) * dr < 0 ||
+      row + (word.length - 1) * dr >= rowsCount ||
+      col + (word.length - 1) * dc < 0 ||
+      col + (word.length - 1) * dc >= colsCount 
+    ){
+      return false
+    }
+
+  // Checks if the word matches in direction
+    for( let i=0; i< word.length; i++) {
+      if(this.letterGrid[row + i * dr][col + 1 * dc] !== word[i]){
+        return false
+      }
+    }
+
+    return true
+
+  };
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
